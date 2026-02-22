fecbd566df548eb3e3a40449298a2e2783890618
samuelcanoterol <samuelcanoterol@gmail.com>
2026-02-22 17:39:40 +0200

    bwi text

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -8,7 +8,7 @@
                     <div class="card-header">Calculator</div>
 
                     <div class="card-body">
-                        <h2>Result is {{ $res }}</h2>
+                        <h2>Your Body Wight Index is {{ $res }}</h2>
                                 <hr>
                                 <a href="{{ route('bwi.index') }}" class="btn btn-success">Back</a>
 
